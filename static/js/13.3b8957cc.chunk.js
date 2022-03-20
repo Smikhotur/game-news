490690135b8d4a02360235aa58f6378415843961
Smikhotur <smikhotur@gmail.com>
2022-03-20 12:44:55 +0200

    Updates

---
+++ static/js/13.3b8957cc.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgame-news"]=this["webpackJsonpgame-news"]||[]).push([[13],{308:function(e,n,t){"use strict";t.r(n);t(0);var i,a,s=t(23),c=t(3),r=t(2),l=t(32),b={};b.ContainerDetails=r.b.section(i||(i=Object(c.a)(["\n  ",";\n"])),Object(s.a)({},l.a)),b.InnerDetails=r.b.div(a||(a=Object(c.a)(["\n  min-height: 100vh;\n  padding: 180px 0;\n"])));var h=t(1);n.default=function(){return Object(h.jsx)(b.ContainerDetails,{children:Object(h.jsx)(b.InnerDetails,{children:Object(h.jsx)("div",{children:"Hello from Details Page"})})})}}}]);
+//# sourceMappingURL=13.3b8957cc.chunk.js.map
