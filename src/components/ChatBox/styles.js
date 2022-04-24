cadbf33f806f88364c12de6fbcfd0d0ce12b45be
Smikhotur <smikhotur@gmail.com>
2022-04-24 21:50:28 +0300

    added search people in messanger

--- src/components/ChatBox/styles.js
+++ src/components/ChatBox/styles.js
@@ -59,12 +59,12 @@ S.ChatInner = styled.div`
   }
 
   &::-webkit-scrollbar-track {
-    -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+    box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
   }
 
   &::-webkit-scrollbar-thumb {
     border-radius: 10px;
-    -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.5);
+    box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.5);
   }
 `;
 
